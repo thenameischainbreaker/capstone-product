6067a62d8558bffb0ec29b5377818fc3990b0383
Jasper Javillo <jasperjavillo@gmail.com>
2023-07-31 11:34:58 -0400

    Created Product.java as an Entity

---
+++ capstone-product/src/main/java/com/capstone/capstoneproduct/entity/Product.java
@@ -0,0 +1,68 @@
+package com.capstone.capstoneproduct.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Product {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int p_id;
+	private String p_name;
+	private String image_id;
+	private double p_price;
+	private String p_description;
+	
+	public Product() {}
+	
+	public Product(int p_id, String p_name, String image_id, double p_price, String p_description)
+	{
+		this.p_id = p_id;
+		this.p_name = p_name;
+		this.image_id = image_id;
+		this.p_price = p_price;
+		this.p_description = p_description;
+	}
+
+	public int getP_id() {
+		return p_id;
+	}
+
+	public void setP_id(int p_id) {
+		this.p_id = p_id;
+	}
+
+	public String getP_name() {
+		return p_name;
+	}
+
+	public void setP_name(String p_name) {
+		this.p_name = p_name;
+	}
+
+	public String getImage_id() {
+		return image_id;
+	}
+
+	public void setImage_id(String image_id) {
+		this.image_id = image_id;
+	}
+
+	public double getP_price() {
+		return p_price;
+	}
+
+	public void setP_price(double p_price) {
+		this.p_price = p_price;
+	}
+
+	public String getP_description() {
+		return p_description;
+	}
+
+	public void setP_description(String p_description) {
+		this.p_description = p_description;
+	}
+}
