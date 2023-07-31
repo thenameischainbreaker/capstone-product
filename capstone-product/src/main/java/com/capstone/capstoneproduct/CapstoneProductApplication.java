6db5f347fe5531cd4bfd71753cadcb5695e301e0
Jasper Javillo <jasperjavillo@gmail.com>
2023-07-31 11:17:44 -0400

    Created capstone-product project

---
+++ capstone-product/src/main/java/com/capstone/capstoneproduct/CapstoneProductApplication.java
@@ -0,0 +1,13 @@
+package com.capstone.capstoneproduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstoneProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapstoneProductApplication.class, args);
+	}
+
+}
---
+++ capstone-product/src/test/java/com/capstone/capstoneproduct/CapstoneProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.capstoneproduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
